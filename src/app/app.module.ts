51e42fc425b5e58817b2e9461768550ae2164ce4
Timothy Alcaide <talcaide@immergis.fr>
2020-09-24 15:29:33 +0200

    feat(router): add custom router state serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,45 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Params, RouterStateSnapshot } from '@angular/router';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import {
+  NgxsRouterPluginModule,
+  RouterStateSerializer,
+} from '@ngxs/router-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+export interface RouterStateParams {
+  url: string;
+  params: Params;
+  queryParams: Params;
+}
+
+// Map the router snapshot to { url, params, queryParams }
+export class CustomRouterStateSerializer
+  implements RouterStateSerializer<RouterStateParams> {
+  serialize(routerState: RouterStateSnapshot): RouterStateParams {
+    const {
+      url,
+      root: { queryParams },
+    } = routerState;
+
+    let { root: route } = routerState;
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const { params } = route;
+
+    return { url, params, queryParams };
+  }
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -34,7 +64,10 @@ import { SharedModule } from './shared/shared.module';
     }),
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
